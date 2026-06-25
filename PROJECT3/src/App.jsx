742bdaca7ef445d84773b09c469f8b04708e65f4
Shini2625 <5260143chandrahasini08@gmail.com>
2026-06-25 10:34:45 +0000

    error msg

--- PROJECT3/src/App.jsx
+++ PROJECT3/src/App.jsx
@@ -10,24 +10,29 @@ function App() {
     watch,
     formState: { errors },
   } = useForm();
-
+  function submitFn(data){
+    alert("you have submitted the form");
+    console.log(data);
+  }
   return (
     <>
      <p>hello this is to understand the react hook form</p>
-     <form onSubmit={handleSubmit((data)=>console.log(data))}>
+     <form onSubmit={handleSubmit(submitFn)}>
       <div>
         <label>First Name:</label>
-        <input {...register('1name')}/>
+        <input {...register('name1')}/>
       </div>
       <br/>
       <div>
         <label>Second Name:</label>
-        <input {...register('2name')}/>
+        <input {...register('name2', {required:true, minLength:{value:5, msg:"enter more than 5 chars"}, maxLength:{value:27, msg:"enter less than 27 chars"}})}/>
+        {errors.name2 && <p>{errors.name2.msg}</p>}
       </div>
-     </form>
-     <button onClick={handleSubmit}>
+     
+     <button type="submit">
        submit
      </button>
+     </form>
     </>
   )
 }

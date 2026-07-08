55e95fe33eb643884a53cdf2c5adce5dcfbc7640
Shini2625 <5260143chandrahasini08@gmail.com>
2026-07-08 11:54:18 +0000

    h2s

--- PROJECT3/src/assets/components/hero2.jsx
+++ PROJECT3/src/assets/components/hero2.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import hero2 from '../images/hero2.png'
-
+import searchImage from '../images/search.png'
 const Hero2 = () => {
   return (
-    <div class="flex-col justify-center p-6">
-        <image src={hero2}/>
-        <div>
-            <button class="border-s-2 rounded-md"> </button>
+    <div class="flex justify-center p-6 py-20">
+        <img class="h-31.25rem w-43rem px-24 " src={hero2}/>
+        <div >
+            <button class="border-2 border-slate-400 rounded-md justify-end"> <img class="h-0.5rem w-0.5rem justify-end" src={searchImage} /> </button>
             <h2 class="text-xl font-bold">Categories</h2>
             <div class="table-row-group text-justify text-gray-400">
                <p>Crafts</p> 

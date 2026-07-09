76692b0e63bb4b344ba8778f1f57f7183c6137bd
Shini2625 <5260143chandrahasini08@gmail.com>
2026-07-09 10:58:23 +0000

    remove node/env tracking

--- PROJECT4/script.js
+++ PROJECT4/script.js
@@ -1,12 +1,18 @@
+require('dotenv').config()
 const express = require('express');
+
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
 
 app.get('/', (req , res) => {
     res.send('Hello! This is your first attempt')
 });
 app.get('/login' , (req , res)=>{
-    res.send('you are trying to login')
+    res.send('<h1>trying to login</h1>')
+});
+
+app.get('/frnd' , (req, res) => {
+    res.send('<h2>your friends list</h2>')
 });
 
 app.listen(port, ()=> {

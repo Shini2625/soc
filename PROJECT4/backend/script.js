1a7389d868fe5e988888c5d9cb297b94869ac095
Shini2625 <5260143chandrahasini08@gmail.com>
2026-07-10 07:57:10 +0000

    trying to add input

--- PROJECT4/backend/script.js
+++ PROJECT4/backend/script.js
@@ -4,6 +4,11 @@ import express from 'express';
 
 const app = express();
 const port = process.env.PORT;
+const quotes = [
+    {id:3 , text:"you r ur own boss"},
+    {id:2 , text:"you r a baddie" },
+    {id:1 , text:"you r gay"}
+];
 
 app.get('/', (req , res) => {
     res.send('Hello! This is your first attempt')
@@ -16,8 +21,9 @@ app.get('/frnd' , (req, res) => {
     res.send('<h2>your friends list</h2>')
 });
 app.get('/quote' , (req , res)=>{
-    
-})
+    res.json(quotes);
+});
+
 app.listen(port, ()=> {
     console.log(`Example app listening on port ${port}`)
 });

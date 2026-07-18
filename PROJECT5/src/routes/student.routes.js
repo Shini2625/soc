be637fc7374a97d9b1090980c49e1074907a1aac
Shini2625 <5260143chandrahasini08@gmail.com>
2026-07-18 12:11:00 +0000

    .

--- PROJECT5/src/routes/student.routes.js
+++ PROJECT5/src/routes/student.routes.js
@@ -2,6 +2,7 @@ import { Router } from "express";
 import { healthCtrl, registerCtrl } from "../controllers/p41.controllers.js";
 import verifyJWT from "../middlewares/auth.middlewares.js"
 import { loginCtrl, refreshAccessTokenCtrl } from "../controllers/login.controllers.js";
+import { logoutCtrl } from "../controllers/logout.controller.js";
 
 const router = Router()
 
@@ -9,7 +10,7 @@ router.route("/health").get(healthCtrl)
 router.route("/register").post(registerCtrl)
 router.route("/login").post( loginCtrl)
 router.route("/refresh-token").post(refreshAccessTokenCtrl)
-
+router.route("/logout").post(verifyJWT, logoutCtrl)
 
 
 
